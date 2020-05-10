1517117f3d78476fb34c3993a96e2c1076df2588
Vincent Chang <vincenttchang90@gmail.com>
2020-05-10 17:37:59 -0400

    chapter 1 exercises

--- chapter_1/chapter_1_exercises.py
+++ chapter_1/chapter_1_exercises.py
@@ -1,2 +1,29 @@
 #chapter 1 exercises
- 
+#1-1
+
+# In a print statement, what happens if you leave out one of the parentheses, or both?
+## print 'hello') or print('hello' return errors
+
+# If you are trying to print a string, what happens if you leave out one of the quotation marks, or both?
+## print('hello) or print(hello') return errors while print(hello) says hello is not defined
+
+#You can use a minus sign to make a negative number like -2. What happens if you put a plus sign before a number? What about 2++2? 
+## 2++2 returns 4, +1+2 returns 3 so it seems you can specify a number is positive with a +
+
+# In math notation, leading zeros are okay, as in 02. What happens if you try this in Python?
+## 2+02 returns an invalid token error
+
+# What happens if you have two value with no operator between them?
+## returns invalid syntax
+
+
+#1-2
+
+# How many seconds are there in 42 minutes 42 seconds?
+## 42*60+42 = 2562
+
+# How many miles are there in 10 kilometers? Hint: there are 1.61 kilometeres in a mile.
+## 10/1.61 = 6.2 miles
+
+# If you run a 10 kilometer race in 42 minutes 52 seconds, what is your average pace? What is your average speed in miles per hour?
+## 6:52 minutes per mile. 8.73 miles per hour
